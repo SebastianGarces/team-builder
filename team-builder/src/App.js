e76e3664cbb685c0c82173f5341caf948cb5ca6b
SebastianGarces <gsgarces1@gmail.com>
2020-02-18 23:36:47 -0500

    completed mvp - working on stretch

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import "./App.css";
+
+import Form, { Button } from "./components/Form/Form";
+
+const MEMBERS_DATA = [
+	{
+		name: "Sebastian Garces",
+		role: "Front End Engineer",
+		email: "gsgarces1@gmail.com"
+	},
+	{
+		name: "John Doe",
+		role: "Back End Engineer",
+		email: "johndoe@gmail.com"
+	},
+	{
+		name: "Marie Doe",
+		role: "UX Designer",
+		email: "mariedoe@gmail.com"
+	},
+	{
+		name: "Tim Doe",
+		role: "CEO",
+		email: "theboss@gmail.com"
+	}
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [members, setMembers] = useState(MEMBERS_DATA);
+	const [memberToEdit, setMemberToEdit] = useState({});
+
+	const addMember = member => {
+		setMembers([...members, member]);
+	};
+
+	const handleEdit = member => {
+		// console.log(member);
+		setMemberToEdit(member);
+	};
+
+	console.log("membertoedit", memberToEdit);
+
+	return (
+		<AppContainer className="App">
+			<Form addMember={addMember} memberToEdit={memberToEdit} />
+			<div className="card-container">
+				{members.map((member, index) => {
+					return (
+						<Card key={index}>
+							<h2>{member.name}</h2>
+							<h4>{`Role: ${member.role}`}</h4>
+							<p>{`Email: ${member.email}`}</p>
+							<Button onClick={() => handleEdit(member)}>
+								Edit
+							</Button>
+						</Card>
+					);
+				})}
+			</div>
+		</AppContainer>
+	);
 }
+
+const AppContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+
+	.card-container {
+		display: flex;
+		flex-wrap: wrap;
+	}
+`;
+
+const Card = styled.div`
+	border: 1px solid #333;
+	padding: 3rem;
+	margin: 3rem;
+`;
 
 export default App;
